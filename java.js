37a0e0320ac1be48684a96c90345b590b431509d
Dursun İNEL <dursun.inel@mrevyap.k12.tr>
2020-05-11 14:57:08 +0300

    4monitöre kargo bilgileri, gezi butonları eklendi

--- java.js
+++ java.js
@@ -13,7 +13,7 @@ function azalt() {
    if (deger!=1) // alınacak ürün miktarının 1 den az olmamasını sağlıyorum.
    document.getElementById('sayi').value = deger - 1;
 }
-
+console.log(window.scrollTo);
 
 btn1.onclick=function(){
     this.style.background="#0069d9";
@@ -40,9 +40,58 @@ btn3.onclick=function(){
     btn2.style.color="#000000";
 }
 
+window.smoothScroll = function (target) {
+    var scrollContainer = target;
+    do { //find scroll container
+        scrollContainer = scrollContainer.parentNode;
+        if (!scrollContainer) return;
+        scrollContainer.scrollTop += 1;
+    } while (scrollContainer.scrollTop == 0);
+
+    var targetY = 0;
+    do { //find the top of target relatively to the container
+        if (target == scrollContainer) break;
+        targetY += target.offsetTop;
+    } while (target = target.offsetParent);
+
+    scroll = function (c, a, b, i) {
+        i++;
+        if (i > 30) return;
+        c.scrollTop = a + (b - a) / 30 * i;
+        setTimeout(function () {
+            scroll(c, a, b, i);
+        }, 20);
+    }
+    // start scrolling
+    scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
+}
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 1000, function(){
+     
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  });
+
 var demo;
 let yeni = document.querySelector("#yorumlar");
-let yeni1 = document.querySelector("#div");
 
 var ad;
 var soyad;
